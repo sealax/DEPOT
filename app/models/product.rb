c33ce2826302c356dba8cc5152eba8a24c3dcc33
Chris Sealey <csealey1@gmail.com>
2026-06-29 15:31:53 +0100

    Adding a cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,43 @@
+#---
+# Excerpted from "Agile Web Development with Rails 8",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit https://pragprog.com/titles/rails8 for more book information.
+#---
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  #...
+
   has_one_attached :image
   after_commit -> { broadcast_refresh_later_to "products" }
   validates :title, :description, :image, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+#
   validates :title, uniqueness: true
   validate :acceptable_image
 
   def acceptable_image
     return unless image.attached?
 
-    acceptable_types = [ "image/gif", "image/jepg", "imag/png" ]
+    acceptable_types = [ "image/gif", "image/jpeg", "image/png" ]
     unless acceptable_types.include?(image.content_type)
-      errors.add(:image, "must be a GIF, JPEG, or PNG")
+      errors.add(:image, "must be a GIF, JPG or PNG image")
     end
   end
+  validates :title, length: { minimum: 10 }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, "Line Items present")
+        throw :abort
+      end
+    end
 end
